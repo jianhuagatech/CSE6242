972f325dfef1c4735cf3727f5363e54bfa83afbc
unknown <huajian.gatech@gamil.com>
2019-11-28 13:51:51 -0500

    adding dashboard

--- src/App.js
+++ src/App.js
@@ -72,7 +72,6 @@ export default function App() {
   }
   function onClick(bed, bath){
     ReactDOM.render(<MapWrapper />, document.getElementById('root'))
-    
   }
   return (
     <Grid container component="main" className={classes.root}>
