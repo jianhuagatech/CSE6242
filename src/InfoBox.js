6106d4fb0f9055fc0612a5a9791f5c64c536f5cd
unknown <wugeyu002@126.com>
2019-12-01 20:44:01 -0500

    adjust infowindow

--- src/InfoBox.js
+++ src/InfoBox.js
@@ -13,7 +13,7 @@ const useStyles = makeStyles(theme => ({
       padding: theme.spacing(2),
       margin: 'auto',
       maxWidth: 800,
-      width: 300,
+      width: 400,
     },
     image: {
       width: 128,
@@ -35,13 +35,13 @@ export default function InfoBox(props) {
         const {rank, uid , selectedLocation, setSelectedLocation} = props;
             
         return (
-            <Box display="flex" justifyContent="flext-start" p={1} m={1}>
-                <Box display="flex" justifyContent="center" alignItems="center" classes = "similarity-rating" borderRadius="50%" width = "42px" height = "42px" bgcolor = "#62aef7" marginRight="2px">
+            <Box display="flex" justifyContent="flext-start" m={1}>
+                <Box display="flex" justifyContent="center" alignItems="center" classes = "similarity-rating" borderRadius="50%" width = "21px" height = "21px" bgcolor = "#62aef7" marginRight="2px">
                     <Box>
-                        <Box textAlign= "center" component="span" display="inline" bgcolor="transparent" fontSize = "20px" color="white" lineHeight = "30px">
+                        <Box textAlign= "center" component="span" display="inline" bgcolor="transparent" fontSize = "10px" color="white" lineHeight = "15px">
                             {rank}
                         </Box>
-                        <Box textAlign= "center" component="span" display="inline" bgcolor="transparent" fontSize = "10px" color="white" lineHeight = "15px">
+                        <Box textAlign= "center" component="span" display="inline" bgcolor="transparent" fontSize = "5px" color="white" lineHeight = "7.5px">
                             /10
                         </Box> 
                     </Box>
@@ -60,7 +60,7 @@ export default function InfoBox(props) {
     return (
         <div className={classes.root}>
             <Paper className={classes.paper}>
-                <Grid container direction="column" spacing={2}>
+                <Grid container spacing={2}>
                     {/* <Grid item>
                         <ButtonBase className={classes.image}>
                             
@@ -70,18 +70,8 @@ export default function InfoBox(props) {
                         <Grid item xs container spacing={1} direction="column">
                             <Grid item xs>
                                 <Box display="flex" justify="space-around">
-                                    <Box>
-                                        <Typography gutterBottom variant="subtitle1">
-                                            {name}
-                                        </Typography>
-                                    </Box>
-                                    <Box display="flex" justifyContent="flext-end">
-                                        <Box textAlign= "center" component="span" display="inline" bgcolor="transparent" fontSize = "16px" color="black" lineHeight = "30px">
-                                            {price}
-                                        </Box>
-                                        <Box textAlign= "center" component="span" display="inline" bgcolor="transparent" fontSize = "10px" color="black" lineHeight = "15px">
-                                            {"$/Month"}
-                                        </Box>
+                                    <Box fontWeight="fontWeightBold" fontSize={24}>
+                                        {name}
                                     </Box>
                                 </Box>
                             </Grid>
@@ -115,6 +105,16 @@ export default function InfoBox(props) {
                                             setSelectedLocation = {setSelectedLocation}/> 
                             </Grid>
                         </Grid>
+                    </Grid>
+                    <Grid>
+                        <Box display="flex" justifyContent="center">
+                            <Box textAlign= "center" component="span" display="inline" bgcolor="transparent" fontSize = "16px" color="black" lineHeight = "30px">
+                                {price}
+                            </Box>
+                            <Box textAlign= "center" component="span" display="inline" bgcolor="transparent" fontSize = "10px" color="black" lineHeight = "15px">
+                                {"$/Month"}
+                            </Box>
+                        </Box>
                     </Grid>
                 </Grid>
             </Paper>
